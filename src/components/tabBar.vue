39e3270930d859b6f7207e88e628af60fb6b3d9d
James Yang <james.y.yang@cargosmart.com>
2019-01-08 13:44:16 +0800

    setup lodash and dateUtil

---
+++ src/components/tabBar.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class='tab-bar'></div>
+</template>
+
+<script>
+	export default {
+		name: "TabBar",
+		data() {
+			return {};
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App'
+import _ from 'lodash';
 
 Vue.config.productionTip = false
 App.mpType = 'app'
+
+Vue.prototype._ = _;
 
 const app = new Vue(App)
 app.$mount()
